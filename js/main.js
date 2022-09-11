fec10ea46f744013d4213c82d493100a5ff7e247
renzien <alifrizki1011@gmail.com>
2022-09-11 10:21:05 +0700

    add: button animation

--- js/main.js
+++ js/main.js
@@ -2,3 +2,11 @@
 window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle('window-scrolled', window.scrollY > 0);
 })
+
+// Contact Button
+const textButtons = document.querySelectorAll('.contact__btn');
+textButtons.forEach(textButton => {
+    let text = textButton.querySelector('p');
+    text.innerHTML = text.innerHTML.split('').map((character, index) => `<span style="transform: rotate(${index * 12}deg)
+    ">${character}</span>`).join('')
+})
